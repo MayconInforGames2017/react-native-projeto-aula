c919d5a9291d0ddeb62ce455b8d647000ebdbad3
Maycon R Santana <31290067+MayconInforGames2017@users.noreply.github.com>
2026-04-28 17:15:34 -0300

    Criando a estrutura do projeto

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import { 
+    View, 
+    Text 
+} from 'react-native'
+
+export default function Index(){
+    return (
+        <View>
+            <Text>Olá, Dev</Text>
+            <Text>React Native</Text>
+        </View>
+    )
+}
